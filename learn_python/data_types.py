e8d2801d0be625afbf480e36d71534a5aac6f441
jahidul2543 <jahidul2543@gmail.com>
2022-05-17 20:05:37 -0400

    python3

---
+++ learn_python/data_types.py
@@ -0,0 +1,20 @@
+name = "Jack's"
+age = 'twenty'
+# method_name()
+# Job of print function is to print hello into the console
+print(name)
+print('Jack')
+type_of_variable_name = type(age)
+print(type_of_variable_name)
+
+school_name = 'izaan_schooliiz'
+print(school_name.capitalize())
+print(len(school_name))
+print(school_name.count('i', 0, 16))
+
+print('My Name: {}'.format(name))
+
+book_list = ['ab','cd', 'de']
+print('Book List: {}'.format(book_list))
+print('Book List Size: {}'.format(len(book_list)))
+
---
+++ learn_python/demo.py
@@ -0,0 +1,13 @@
+'''
+Learning Python Function
+'''
+
+
+def add():
+    a  = 2
+    b = 3
+    c = a + b
+    print('Result: {}'.format(c))
+
+
+add()
