fc6ad44e82a66c921f80bd7b69fe0ebaee201599
jahidul2543 <jahidul2543@gmail.com>
2022-05-19 19:40:03 -0400

    python 102

---
+++ learn_python/atm.py
@@ -0,0 +1,9 @@
+def insert_card(visa_card):
+    print('Thank for inserting a valid card: {}'.format(visa_card))
+    return 'Write your credentials'
+
+
+if __name__ == "__main__":
+    insert_card('my-visa-card')
+
+
--- learn_python/lambda_function.py
+++ learn_python/lambda_function.py
@@ -12,7 +12,8 @@ How to get a value from a list?
 """
 
 
-def add():
+def add(event, context):
+    print('Lambd Event {}:'.format(event))
     a = 2
     b = 3
     c = a + b
@@ -20,14 +21,14 @@ def add():
     return c
 
 
-def deduct():
+def deduct(event, context):
     # list_of_arg = sys.argv
     # print("List if argument: {}".format(list_of_arg))
     # a = int(list_of_arg[1])  # int,  str --> Command line arg is str type, lets convert it to int
     # print('Type of var  a: {}'.format(type(a)))
     # b = int(list_of_arg[2])
-    a = os.environ['a']
-    b = os.environ['b']
+    a = int(os.environ['value1'])
+    b = int(os.environ['value2'])
     c = b - a
     print('Result: {}'.format(c))
 
@@ -38,12 +39,27 @@ def multiplication(a, b):
     return result
 
 
-if __name__ == "__main__":
-    # return_value = add()
-    # print('Return Value {}'.format(return_value))
-    # deduct()
-    returned_value = multiplication(a=200, b=4)
-    print(returned_value)
+# if __name__ == "__main__":
+#     return_value = add()
+#     print('Return Value {}'.format(return_value))
+#     deduct()
+#     returned_value = multiplication(a=200, b=4)
+#     print(returned_value)
 
 
 
+
+
+
+
+
+
+
+# import json
+
+# def lambda_handler(event, context):
+#     # TODO implement
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps('Hello from Lambda!')
+#     }
